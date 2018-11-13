beb4f327f0be9ff47dbbed159b01e5d92a34a6f9
Jennifer Rubin <jenlrubin9326@gmail.com>
2018-11-13 14:36:04 -0500

    hardcoded location

--- assets/javascript.js
+++ assets/javascript.js
@@ -3,9 +3,12 @@ var googleURL = "https://maps.googleapis.com/maps/api/geocode/json"
 var location =  $("#locationtxt").val().trim()
 var latitude;
 var longitude; 
+//n = 10, number of movie theaters that link will return 
+var moviegluURL = "https://api-gate2.movieglu.com/"
+
 //making a GET request to get user lat & long from user location
 function geolocation() {
-    
+    location = '20 W 34th St, New York, NY 10001'
    
     axios.get(googleURL, {
         params: {
@@ -27,3 +30,14 @@ function geolocation() {
 //call function
 
 geolocation(); 
+
+//use user geolocation in movieglu api to find cinemas nearby
+
+findCinemas = () => {
+    axios.get(moviegluURL, {
+        params : {
+            key: 'Au2GVSQyKQ3qZ7Ak0PY7alzjTHNGkle6EbodvsJb'
+
+        }
+    })
+}

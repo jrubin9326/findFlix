7c68a47677741a9ce74b5da576474bb88422c13e
Robert Flanagan <robertflanagan95@gmail.com>
2018-11-14 20:40:40 -0500

    pushing TMDB API response to the page for each genre

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,13 +1,13 @@
 $(".img-thumbnail").on("click", function() {
+    $("#movie-results").empty();
     var searchTerm = $(this).attr("value");
     console.log(searchTerm);
 
-    displayMovieInfo(searchTerm);
-});
+    // `movieData` will hold the response object
+    var movieData;
 
-function displayMovieInfo(search) {
     // `search` corresponds to the genre code for the TMDB API
-    var queryURL = "https://api.themoviedb.org/3/discover/movie?api_key=6d9d4c1511419d5253e7cf5683b3e1df&with_genres=" + search + "&sort_by=popularity.desc";
+    var queryURL = "https://api.themoviedb.org/3/discover/movie?api_key=6d9d4c1511419d5253e7cf5683b3e1df&with_genres=" + searchTerm + "&sort_by=popularity.desc";
 
     // Creates AJAX call for the specific movie button being clicked
     $.ajax({
@@ -16,7 +16,45 @@ function displayMovieInfo(search) {
     }).then(function(response) {
         // print movie list to console
         console.log(response.results);
+        movieData = response.results;
+
+        // show 10 results on the page
+        for (var i = 0; i < 10; i ++) {
+            displayMovieInfo(movieData[i]);
+        }
     });
+});
+
+function displayMovieInfo(search) {
+    console.log(search.overview);
+    console.log(search.release_date);
+    console.log(search.popularity);
+    console.log(search.vote_average);
+
+    var newMovieDiv = $("<div>");
+
+    var newTitle = $("<h5>");
+    newTitle.text(search.title);
+    
+    var newOverview = $("<p>");
+    newOverview.text(search.overview);
+    
+    var newRelease = $("<p>");
+    newRelease.text("Release: " + search.release_date);
+    
+    var newPopularity = $("<p>");
+    newPopularity.text("Popularity: " + search.popularity);
+
+    var newAvgVote = $("<p>");
+    newAvgVote.text("Average Vote: " + search.vote_average);
+
+    newMovieDiv.append(newTitle);
+    newMovieDiv.append(newOverview);
+    newMovieDiv.append(newRelease);
+    newMovieDiv.append(newPopularity);
+    newMovieDiv.append(newAvgVote);
+
+    $("#movie-results").append(newMovieDiv);
 }
 
 //global variables
